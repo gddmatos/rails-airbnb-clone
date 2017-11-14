a7af8be2377021d21a5eb40f15a01b5cdc235f12
Hamish Jolly <jolham1985@gmail.com>
2017-11-14 12:04:46 +0000

    seeds file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Chateau.destroy_all
+
+puts 'Showing Homes...'
+restaurants_attributes = [
+  {
+
+    address:      "Copse Hill",
+    description:  "In The Heart Of Wimbledon Village",
+    price:        12000,
+    area:         7800
+  },
+  {
+
+    address:      "Highbury Road",
+    description:   "Pure elegance"
+    price:        10000,
+    area:         65000
+  },
+  {
+
+    address:      "Homefield Road",
+    description:  "Distinguished flat , fantatsic location"
+    price:         7000,
+    area:          3000
+  },
+{
+
+    address:      "Homepark",
+    description:  "This is Living"
+    price:         15000,
+    area:          7500
+  },
+{
+
+    address:      "Roedan Crescent",
+    description:  "Absolute decadance"
+    price:         20000,
+    area:          11300
+  },
+]
+Chateau.create!(chateuas_attributes)
+puts 'Finished!'
