5b6701843aa37775a8f608fafc12ca33a6a2801b
Marianne Delaforge <marianne.delaforge@gmail.com>
2017-11-15 10:28:57 +0000

    booking form + def create to review

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :chateaus
-  resources :bookings
+  resources :chateaus do
+    resources :bookings, only: [:create]
+  end
+
+  resources :bookings, except: [:create]
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
