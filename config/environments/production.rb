eb62f94d99bc0b6264abb445a016bfa9394f16f0
Hamish Jolly <jolham1985@gmail.com>
2017-11-16 14:16:11 +0000

    mailers finished

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,14 @@ Rails.application.configure do
 
     config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "lepetitchateau.herokuapp.com" }
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: 'gmail.com',
+  user_name: ENV['GMAIL_ADDRESS'],
+  password: ENV['GMAIL_APP_PASSWORD'],
+  authentication: :login,
+  enable_starttls_auto: true  }
 end
